b993393c51e3df7246a3a3b525e86c64a0db91da
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 05:17:56 -0300

    Update NeoSkelEscottor.cs

--- script/monster/undead/NeoSkelEscottor.cs
+++ script/monster/undead/NeoSkelEscottor.cs
@@ -13,7 +13,7 @@ namespace LKCamelot.script.monster
         public override int Dam { get { return 65; } }
         public override int AC { get { return 35; } }
         public override int Hit { get { return 55; } }
-        public override int XP { get { return 515; } }
+        public override int XP { get { return 950; } }
         public override int Color { get { return 1; } }
         public override int SpawnTime { get { return 120000; } }
         public override Race Race { get { return Race.Undead; } }
@@ -24,11 +24,13 @@ namespace LKCamelot.script.monster
             {
                 return new LootPack(new LootPackEntry[]
                 {
-                    new LootPackEntry(5.0, typeof(script.item.Mantle), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(5.0, typeof(script.item.OneEdgedAxe), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(5.0, typeof(script.item.Mask), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(5.0, typeof(script.item.SquareShield), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(5.0, typeof(script.item.Epee), "10d22+250", 40, 1, 1),
+                    
+                    new LootPackEntry(5.0, typeof(script.item.Saber), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(5.0, typeof(script.item.Simitar), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(5.0, typeof(script.item.TriangleShield), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(5.0, typeof(script.item.Mace), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(5.0, typeof(script.item.Cloak), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(5.0, typeof(script.item.Mantle), "10d22+250", 40, 1, 1).
                     new LootPackEntry(25.0, typeof(script.item.Gold), "10d30+550", 40, 1, 1),
                 });
             }
