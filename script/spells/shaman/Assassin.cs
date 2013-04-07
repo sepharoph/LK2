d6c07f3ad451dda4ee1b966a83195fa736ee36f7
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 19:25:18 -0300

    Update Assassin.cs

--- script/spells/shaman/Assassin.cs
+++ script/spells/shaman/Assassin.cs
@@ -11,7 +11,7 @@
         public override int ManaCost { get { return -78; } } //78
         public override int ManaCostPl { get { return 6; } }
         public override LKCamelot.library.Class ClassReq { get { return LKCamelot.library.Class.Shaman; } }
-        public override int menCoff { get { return 12; } }
+        public override int menCoff { get { return 8; } }
         public override SpellSequence Seq
         {
             get
