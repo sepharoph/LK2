2aef0fae9524344f1d57eb02bfe7a9f09fe0003e
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 01:58:42 -0300

    Update ItemZombie.cs

--- script/monster/undead/ItemZombie.cs
+++ script/monster/undead/ItemZombie.cs
@@ -33,10 +33,10 @@ namespace LKCamelot.script.monster
                     new LootPackEntry(2, typeof(script.item.Cloak), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.Hanger), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.Mantle), "15d10+225", 1, 1, 1),
-                    new LootPackEntry(2, typeof(script.item.Saver), "15d10+225", 1, 1, 1),
+                    new LootPackEntry(2, typeof(script.item.Saber), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.Simitar), "15d10+225", 1, 1, 1),
                     new LootPackEntry(25.0, typeof(script.item.Gold), "5d10+1200", 40, 1, 1),
-                    // shouldn't drop book of trace , (Saver should = Saber)
+                    // shouldn't drop book of trace ,
                     // should drop Book of Comeback
                 });
             }
