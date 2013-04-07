d84effab288a4a88edd58362259bd69fc6476bfc
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 00:33:19 -0300

    Update Skewer.cs

--- script/item/weapons/spear/Skewer.cs
+++ script/item/weapons/spear/Skewer.cs
@@ -8,14 +8,16 @@ namespace LKCamelot.script.item
         public override string Name { get { return "Skewer"; } }
 
         public override int DamBase { get { return 94; } }
-        public override int ACBase { get { return 0; } }
+        public override int ACBase { get { return 35; } }
+        public override int InitMaxMana { get { return 56; } }//add max mana plz approve!!
 
         public override int StrReq { get { return 54; } }
         public override int MenReq { get { return 120; } }
         public override int DexReq { get { return 0; } }
-        public override int ReduceCast { get { return 300; } }
-        public override int InitMinHits { get { return 80; } }
-        public override int InitMaxHits { get { return 80; } }
+        
+        public override int ReduceCast { get { return 600; } }
+        public override int InitMinHits { get { return 44; } }//adds 44 HIT
+        public override int InitMaxHits { get { return 44; } }//adds 44 HIT
         public override int BuyPrice { get { return 100000; } }
 
         public override Class ClassReq { get { return Class.Shaman; } }
