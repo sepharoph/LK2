ec85c21a9ad7033f37df8508670d52d9ba6fdb1d
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 01:22:28 -0300

    Update WarPike.cs

--- script/item/weapons/spear/WarPike.cs
+++ script/item/weapons/spear/WarPike.cs
@@ -19,7 +19,8 @@ namespace LKCamelot.script.item
         public override int ReduceCast { get { return 1000; } }
         public override int InitMinHits { get { return 95; } }//adds 95 HIT
         public override int InitMaxHits { get { return 95; } }//adds 95 HIT
-        public override int BuyPrice { get { return 5000; } }
+        public override int BuyPrice { get { return 150000; } }
+        public override int SellPrice { get { return 75000; } }
 
         public override Class ClassReq { get { return Class.Shaman; } }
         public override WeaponType WeaponType { get { return WeaponType.Spear2; } }
