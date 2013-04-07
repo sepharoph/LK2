a483a177fd0151846be5d21ece733d042499da5d
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 00:09:40 -0300

    Update Cane.cs

--- script/item/weapons/cane/Cane.cs
+++ script/item/weapons/cane/Cane.cs
@@ -9,14 +9,17 @@ namespace LKCamelot.script.item
 
         public override int DamBase { get { return 52; } }
         public override int ACBase { get { return 0; } }
-
+        public override int InitMaxMana { get { return 99; } }//add max mana plz approve!!
+        
         public override int StrReq { get { return 55; } }
         public override int MenReq { get { return 245; } }
         public override int DexReq { get { return 62; } }
         public override int LevelReq { get { return 36; } }
+        
+        
         public override int ReduceCast { get { return 900; } }
-        public override int InitMinHits { get { return 80; } }
-        public override int InitMaxHits { get { return 80; } }
+        public override int InitMinHits { get { return 36; } }//adds 36 hit
+        public override int InitMaxHits { get { return 36; } }//adds 36 hit
         public override int BuyPrice { get { return 5000; } }
 
         public override Class ClassReq { get { return Class.Wizard; } }
