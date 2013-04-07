c69b51544630baf5c343ec68b380bd639bd24bea
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 00:16:22 -0300

    Update LongStaff.cs

--- script/item/weapons/cane/LongStaff.cs
+++ script/item/weapons/cane/LongStaff.cs
@@ -9,13 +9,15 @@ namespace LKCamelot.script.item
 
         public override int DamBase { get { return 107; } }
         public override int ACBase { get { return 82; } }
-//add +64 hit
+        public override int InitMaxMana { get { return 64; } }//add max mana plz approve!!
+        
         public override int StrReq { get { return 72; } }
         public override int MenReq { get { return 275; } }
         public override int DexReq { get { return 0; } }
+        
         public override int ReduceCast { get { return 800; } }
-        public override int InitMinHits { get { return 80; } }
-        public override int InitMaxHits { get { return 80; } }
+        public override int InitMinHits { get { return 64; } }//adds 64 hit
+        public override int InitMaxHits { get { return 64; } }//adds 64 hit
         public override int BuyPrice { get { return 5000; } }
         public override int SellPrice { get { return 2500; } }
 
