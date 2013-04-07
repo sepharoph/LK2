e0ba3fa1384cd15659383e65cc81ecb78d3e8dd5
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 02:05:32 -0300

    Update BastardSword.cs

--- script/item/weapons/sword/BastardSword.cs
+++ script/item/weapons/sword/BastardSword.cs
@@ -16,6 +16,8 @@ namespace LKCamelot.script.item
 		public override int InitMinHits { get { return 800; } }
 		public override int InitMaxHits { get { return 800; } }
 
+		public override int SellPrice { get { return 15000; } }
+
 		public override Class ClassReq { get { return Class.Knight | Class.Swordsman; } }
 		public override WeaponType WeaponType { get { return WeaponType.Sword; } }
 
