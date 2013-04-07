d8f181c6fedd56fe47127a61a5ac84a5f951a2a7
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-06 23:55:52 -0300

    Update Kassandra.cs

--- script/item/weapons/cane/Kassandra.cs
+++ script/item/weapons/cane/Kassandra.cs
@@ -9,15 +9,15 @@ namespace LKCamelot.script.item
 
         public override int DamBase { get { return 150; } }
         public override int ACBase { get { return 170; } }
-//add +150 to hit
 
         public override int StrReq { get { return 200; } }
         public override int MenReq { get { return 4500; } }
         public override int DexReq { get { return 5500; } }
         public override int LevelReq { get { return 240; } }//reflects for 7th promo
 
-        public override int InitMinHits { get { return 80; } }
-        public override int InitMaxHits { get { return 80; } }
+        public override int InitMinHits { get { return 150; } }
+        public override int InitMaxHits { get { return 150; } }//reflects +150hit for while item equipped
+        
         public override int BuyPrice { get { return 5000; } }
         public override int ReduceCast { get { return 1150; } }
 
