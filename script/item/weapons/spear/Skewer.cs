b9280e51b9db92fa83a47c0b31f2e6811cb864a5
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 01:20:14 -0300

    Update Skewer.cs

--- script/item/weapons/spear/Skewer.cs
+++ script/item/weapons/spear/Skewer.cs
@@ -18,7 +18,8 @@ namespace LKCamelot.script.item
         public override int ReduceCast { get { return 600; } }
         public override int InitMinHits { get { return 44; } }//adds 44 HIT
         public override int InitMaxHits { get { return 44; } }//adds 44 HIT
-        public override int BuyPrice { get { return 100000; } }
+        public override int BuyPrice { get { return 10000; } }
+        public override int BuyPrice { get { return 5000; } }
 
         public override Class ClassReq { get { return Class.Shaman; } }
         public override WeaponType WeaponType { get { return WeaponType.Spear; } }
