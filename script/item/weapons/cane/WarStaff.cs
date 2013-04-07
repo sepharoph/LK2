6512679e54fef79fa89102f2fc381591e159325e
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 00:26:06 -0300

    Update WarStaff.cs

--- script/item/weapons/cane/WarStaff.cs
+++ script/item/weapons/cane/WarStaff.cs
@@ -9,15 +9,15 @@ namespace LKCamelot.script.item
 
         public override int DamBase { get { return 146; } }
         public override int ACBase { get { return 67; } }
-//need to add to hit 89
-//need to add to max mana 122        
+        public override int InitMaxMana { get { return 122; } }//add max mana plz approve!!
+        
         public override int StrReq { get { return 88; } }
         public override int MenReq { get { return 341; } }
         public override int DexReq { get { return 0; } }
         public override int ReduceCast { get { return 900; } }//updated post 1500cast speed
 
-        public override int InitMinHits { get { return 80; } }
-        public override int InitMaxHits { get { return 80; } }
+        public override int InitMinHits { get { return 89; } }//adds 89 HIT
+        public override int InitMaxHits { get { return 89; } }//adds 89 HIT
         public override int BuyPrice { get { return 25000; } }
         public override int SellPrice { get { return 12500; } }
 
