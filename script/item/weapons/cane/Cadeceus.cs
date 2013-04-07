9d1fd758ef92b762ee597d7ae32a47188ef60e88
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 00:06:21 -0300

    Update Cadeceus.cs

--- script/item/weapons/cane/Cadeceus.cs
+++ script/item/weapons/cane/Cadeceus.cs
@@ -9,7 +9,7 @@ namespace LKCamelot.script.item
 
         public override int DamBase { get { return 91; } }
         public override int ACBase { get { return 0; } }
-//add +220 max mana
+        public override int InitMaxMana { get { return 220; } }//add max mana plz approve!!
 
         public override int StrReq { get { return 125; } }
         public override int MenReq { get { return 512; } }
@@ -17,8 +17,8 @@ namespace LKCamelot.script.item
         public override int ReduceCast { get { return 1100; } }
         public override int LevelReq { get { return 160; } }//reflects item is for 3rd promo
         
-        public override int InitMinHits { get { return 80; } }
-        public override int InitMaxHits { get { return 80; } }
+        public override int InitMinHits { get { return 0; } }//Doesn't add HIT
+        public override int InitMaxHits { get { return 0; } }//Doesn't add HIT
         public override int BuyPrice { get { return 5000; } }
 
         public override Class ClassReq { get { return Class.Wizard; } }
