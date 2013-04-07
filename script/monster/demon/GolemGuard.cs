e6e2b89f991b914afbd108d95870cbabcbcd52ab
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 19:56:23 -0300

    Update GolemGuard.cs

--- script/monster/demon/GolemGuard.cs
+++ script/monster/demon/GolemGuard.cs
@@ -25,15 +25,15 @@ namespace LKCamelot.script.monster
             {
                 return new LootPack(new LootPackEntry[]
                 {
-                    new LootPackEntry(0.1, typeof(script.item.UltraBigBangBook), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(0.1, typeof(script.item.MagmaHandBook), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(0.15, typeof(script.item.ThunderStormBook), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(0.15, typeof(script.item.RevelationBook), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(0.15, typeof(script.item.BigBangBook), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(0.2, typeof(script.item.FullPlate), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(0.2, typeof(script.item.GreatSword), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(0.2, typeof(script.item.WidePlate), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(0.2, typeof(script.item.GreatMaul), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.01, typeof(script.item.UltraBigBangBook), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.01, typeof(script.item.MagmaHandBook), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.019, typeof(script.item.ThunderStormBook), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.019, typeof(script.item.RevelationBook), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.019, typeof(script.item.BigBangBook), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.02, typeof(script.item.FullPlate), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.02, typeof(script.item.GreatSword), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.02, typeof(script.item.WidePlate), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.02, typeof(script.item.GreatMaul), "10d22+250", 40, 1, 1),
                     new LootPackEntry(15.0, typeof(script.item.Gold), "10d30+550", 40, 1, 1),
                 });
             }
