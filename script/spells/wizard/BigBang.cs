37a608f10b780be2a923349a37cc55a5ad294414
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-06 21:36:37 -0300

    Update BigBang.cs

--- script/spells/wizard/BigBang.cs
+++ script/spells/wizard/BigBang.cs
@@ -6,11 +6,12 @@
         public override int SpellLearnedIcon { get { return 64; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
-        public override int DamBase { get { return 488; } }
-        public override int DamPl { get { return 12; } }
+        public override int DamBase { get { return 508; } }//compared to assassin
+        public override int DamPl { get { return 13; } }//compared to assassin
         public override int ManaCost { get { return -77; } }
         public override int ManaCostPl { get { return 6; } }
         public override LKCamelot.library.Class ClassReq { get { return LKCamelot.library.Class.Wizard; } }
+        public override int menCoff { get { return 12; } }//compared to assassin
         public override SpellSequence Seq
         {
             get
