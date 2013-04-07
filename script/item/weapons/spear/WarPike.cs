88ac05d1177c2bddf70acf7a05a4e988ea3fe7b5
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 00:28:54 -0300

    Update WarPike.cs

--- script/item/weapons/spear/WarPike.cs
+++ script/item/weapons/spear/WarPike.cs
@@ -9,16 +9,16 @@ namespace LKCamelot.script.item
 
         public override int DamBase { get { return 166; } }
         public override int ACBase { get { return 78; } }
-        public override int ReduceCast { get { return 1000; } }
+        public override int InitMaxMana { get { return 132; } }//add max mana plz approve!!
 
         public override int StrReq { get { return 138; } }
         public override int MenReq { get { return 310; } }
         public override int DexReq { get { return 208; } }
-        public override int LevelReq { get { return 100; } }//item is for after first promotion
+        public override int LevelReq { get { return 100; } }//item is comparable to Grand Staff
         
-        
-        public override int InitMinHits { get { return 80; } }
-        public override int InitMaxHits { get { return 80; } }
+        public override int ReduceCast { get { return 1000; } }
+        public override int InitMinHits { get { return 95; } }//adds 95 HIT
+        public override int InitMaxHits { get { return 95; } }//adds 95 HIT
         public override int BuyPrice { get { return 5000; } }
 
         public override Class ClassReq { get { return Class.Shaman; } }
