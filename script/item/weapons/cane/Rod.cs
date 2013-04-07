a7a0f191bc984c11e326b079538b63064af9ea22
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 00:21:02 -0300

    Update Rod.cs

--- script/item/weapons/cane/Rod.cs
+++ script/item/weapons/cane/Rod.cs
@@ -9,15 +9,15 @@ namespace LKCamelot.script.item
 
         public override int DamBase { get { return 21; } }
         public override int ACBase { get { return 12; } }
-//add +8 hit
-//add +26 max mana
+        public override int InitMaxMana { get { return 26; } }//add max mana plz approve!!
+
         public override int StrReq { get { return 18; } }
         public override int MenReq { get { return 51; } }
         public override int DexReq { get { return 0; } }
 
         public override int ReduceCast { get { return 200; } }
-        public override int InitMinHits { get { return 80; } }
-        public override int InitMaxHits { get { return 80; } }
+        public override int InitMinHits { get { return 8; } }//adds 8 HIT
+        public override int InitMaxHits { get { return 8; } }//adds 8 HIT
         public override int BuyPrice { get { return 2000; } }
         public override int BuyPrice { get { return 1000; } }        
 
