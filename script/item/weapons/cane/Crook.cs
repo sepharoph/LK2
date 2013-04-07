663990c6691bf3a6431d90afc6bf2c5777801e89
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 01:32:11 -0300

    Update Crook.cs

--- script/item/weapons/cane/Crook.cs
+++ script/item/weapons/cane/Crook.cs
@@ -20,6 +20,7 @@ namespace LKCamelot.script.item
         public override int InitMinHits { get { return 0; } }//adds 0 hit
         public override int InitMaxHits { get { return 0; } }//adds 0 hit
         public override int BuyPrice { get { return 100000; } }
+        public override int SellPrice { get { return 50000; } }
 
         public override Class ClassReq { get { return Class.Wizard; } }
         public override WeaponType WeaponType { get { return WeaponType.Cane; } }
