0116d03bef7a9c3270998a005e5c950f6b7927ae
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 00:39:07 -0300

    Update Javelin.cs

--- script/item/weapons/spear/Javelin.cs
+++ script/item/weapons/spear/Javelin.cs
@@ -8,14 +8,17 @@ namespace LKCamelot.script.item
         public override string Name { get { return "Javelin"; } }
 
         public override int DamBase { get { return 145; } }
-        public override int ACBase { get { return 45; } }
+        public override int ACBase { get { return 104; } }
+        public override int InitMaxMana { get { return 99; } }//add max mana plz approve!!
 
         public override int StrReq { get { return 116; } }
         public override int MenReq { get { return 270; } }
         public override int DexReq { get { return 0; } }
-        public override int ReduceCast { get { return 300; } }
-        public override int InitMinHits { get { return 80; } }
-        public override int InitMaxHits { get { return 80; } }
+        public override int LevelReq { get { return ; } }//reflects item is for 3rd promo
+        
+        public override int ReduceCast { get { return 800; } }//8 dec time
+        public override int InitMinHits { get { return 80; } }//adds 80 HIT
+        public override int InitMaxHits { get { return 80; } }//adds 80 HIT
         public override int BuyPrice { get { return 5000; } }
 
         public override Class ClassReq { get { return Class.Shaman; } }
