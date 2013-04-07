9763baad909b028a54bc1a1cb032b3368ae28fbb
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 02:05:48 -0300

    Update SamuraiSword.cs

--- script/item/weapons/sword/SamuraiSword.cs
+++ script/item/weapons/sword/SamuraiSword.cs
@@ -15,6 +15,8 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 800; } }
 		public override int InitMaxHits { get { return 800; } }
+		
+		public override int SellPrice { get { return 25000; } }
 
 		public override Class ClassReq { get { return Class.Swordsman; } }
 		public override WeaponType WeaponType { get { return WeaponType.Sword; } }
