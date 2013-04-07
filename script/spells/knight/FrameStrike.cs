0c1160d58dc88239098f4bd7638b818bd6536eab
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-06 21:30:14 -0300

    Update FrameStrike.cs
    updated need animation still

--- script/spells/knight/FrameStrike.cs
+++ script/spells/knight/FrameStrike.cs
@@ -6,10 +6,12 @@
         public override int SpellLearnedIcon { get { return 67; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
-        public override int DamBase { get { return 152; } }
-        public override int DamPl { get { return 8; } }
-        public override int ManaCost { get { return 102; } }
-        public override int ManaCostPl { get { return 6; } }
+        public override int DamBase { get { return 2000; } }//updated
+        public override int DamPl { get { return 50; } }//updated
+        public override int ManaCost { get { return 100; } }//updated
+        public override int ManaCostPl { get { return 5; } }//updated
+        public override int RecastTime { get { return 2000; } }//updated
+        public override int strCoff { get { return 2; } }//attempted this line
         public override LKCamelot.library.Class ClassReq { get { return LKCamelot.library.Class.Knight; } }
         public override SpellSequence Seq
         {
