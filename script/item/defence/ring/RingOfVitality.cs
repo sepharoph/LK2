024f3151f830725a2fcde17319ad0dd28036ec9f
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 19:02:32 -0300

    Update RingOfVitality.cs

--- script/item/defence/ring/RingOfVitality.cs
+++ script/item/defence/ring/RingOfVitality.cs
@@ -8,7 +8,9 @@ namespace LKCamelot.script.item
         public override string Name { get { return "Ring of Vitality"; } }
 
         public override int DamBase { get { return 0; } }
-        public override int ACBase { get { return 150; } }
+        public override int ACBase { get { return 75; } }
+        public override int InitMaxVit { get { return 25; } } //adds 25Vit
+        
 
         public override int StrReq { get { return 0; } }
         public override int DexReq { get { return 0; } }
@@ -17,7 +19,7 @@ namespace LKCamelot.script.item
         public override int InitMaxHits { get { return 65; } }
         public override int BuyPrice { get { return 8000; } }
 
-        public override Class ClassReq { get { return 40; } }
+        public override Class ClassReq { get { return 240; } }//6th promo item
         public override ArmorType ArmorType { get { return ArmorType.Ring; } }
 
         public RingOfVitality()
