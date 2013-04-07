de3410b71b92860331771a4860117daae631e6a1
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 01:25:27 -0300

    Update ForkedSpear.cs

--- script/item/weapons/spear/ForkedSpear.cs
+++ script/item/weapons/spear/ForkedSpear.cs
@@ -14,13 +14,13 @@ namespace LKCamelot.script.item
         public override int StrReq { get { return 169; } }
         public override int MenReq { get { return 381; } }
         public override int DexReq { get { return 243; } }
-        public override int LevelReq { get { return 100; } }//reflects item is for 3rd promo
+        public override int LevelReq { get { return 140; } }//reflects item is for 3rd promo
         
         public override int ReduceCast { get { return 1100; } }
         public override int InitMinHits { get { return 0; } }//adds 0 hit
         public override int InitMaxHits { get { return 0; } }//adds 0 hit
-        public override int BuyPrice { get { return 200000; } }
-        public override int SellPrice { get { return 100000; } }
+        public override int BuyPrice { get { return 250000; } }
+        public override int SellPrice { get { return 150000; } }
 
         public override Class ClassReq { get { return Class.Shaman; } }
         public override WeaponType WeaponType { get { return WeaponType.Spear; } }
