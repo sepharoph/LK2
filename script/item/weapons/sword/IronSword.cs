318b798a4c21f9b7f8f2328b7cb7466be4049c38
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 01:59:38 -0300

    Update IronSword.cs

--- script/item/weapons/sword/IronSword.cs
+++ script/item/weapons/sword/IronSword.cs
@@ -15,6 +15,8 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 420; } }
 		public override int InitMaxHits { get { return 420; } }
+		
+		public override int SellPrice { get { return 2500; } }
 
 		public override Class ClassReq { get { return Class.Knight | Class.Swordsman; } }
 		public override WeaponType WeaponType { get { return WeaponType.Sword; } }
