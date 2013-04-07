af0d6235e56c8eaef549168fbb0a1df946d8e4a8
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 01:43:06 -0300

    Update BarbarianAxe.cs

--- script/item/weapons/axe/BarbarianAxe.cs
+++ script/item/weapons/axe/BarbarianAxe.cs
@@ -16,6 +16,8 @@ namespace LKCamelot.script.item
         public override int InitMinHits { get { return 700; } }
         public override int InitMaxHits { get { return 700; } }
 
+        public override int SellPrice { get { return 100000; } }
+
         public override Class ClassReq { get { return Class.Knight | Class.Swordsman; } }
         public override WeaponType WeaponType { get { return WeaponType.Axe; } }
 
