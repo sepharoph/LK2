f64c0c630845ffb0ce3eea9cdccd39ff30e8a7af
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 02:04:38 -0300

    Update Claymore.cs

--- script/item/weapons/sword/Claymore.cs
+++ script/item/weapons/sword/Claymore.cs
@@ -15,6 +15,8 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 550; } }
 		public override int InitMaxHits { get { return 550; } }
+		
+		public override int SellPrice { get { return 10000; } }
 
 		public override Class ClassReq { get { return Class.Knight | Class.Swordsman; } }
 		public override WeaponType WeaponType { get { return WeaponType.Sword; } }
