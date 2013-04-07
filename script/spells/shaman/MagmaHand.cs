3fac66d806272e50bc76eb15a736c617f008147c
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-06 22:52:51 -0300

    Update MagmaHand.cs

--- script/spells/shaman/MagmaHand.cs
+++ script/spells/shaman/MagmaHand.cs
@@ -6,12 +6,12 @@
         public override int SpellLearnedIcon { get { return 70; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
-        public override int DamBase { get { return 536; } }//supposed to do 2nd most single target damage
-        public override int DamPl { get { return 14; } }
+        public override int DamBase { get { return 778; } }//supposed to do 2nd most single target damage
+        public override int DamPl { get { return 16; } }
         public override int ManaCost { get { return -96; } }//compared with other spells
         public override int ManaCostPl { get { return 8; } }//compared with other spells
         public override LKCamelot.library.Class ClassReq { get { return LKCamelot.library.Class.Shaman; } }
-        public override int menCoff { get { return 14; } }//strong spell
+        public override int menCoff { get { return 12; } }//strong spell
         public override SpellSequence Seq
         {
             get
