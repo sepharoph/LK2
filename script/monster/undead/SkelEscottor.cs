67578760720d125e96596f8d4ef1bf6649778928
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 05:20:55 -0300

    Update SkelEscottor.cs

--- script/monster/undead/SkelEscottor.cs
+++ script/monster/undead/SkelEscottor.cs
@@ -25,9 +25,10 @@ namespace LKCamelot.script.monster
                 return new LootPack(new LootPackEntry[]
                 {
                     new LootPackEntry(0.1, typeof(script.item.Saber), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(0.1, typeof(script.item.Rapier), "10d22+250", 40, 1, 1),
-                    new LootPackEntry(0.1, typeof(script.item.SpikedClub), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.1, typeof(script.item.Cloak), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.1, typeof(script.item.TriangleShield), "10d22+250", 40, 1, 1),
                     new LootPackEntry(0.1, typeof(script.item.Cape), "10d22+250", 40, 1, 1),
+                    new LootPackEntry(0.1, typeof(script.item.Helmet), "10d22+250", 40, 1, 1),
                     new LootPackEntry(0.1, typeof(script.item.SmallAxe), "10d22+250", 40, 1, 1),
                     new LootPackEntry(15.0, typeof(script.item.Gold), "10d24+300", 40, 1, 1),
                 });
