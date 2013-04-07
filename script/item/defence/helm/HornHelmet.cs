c00f1edae7ec622f143afa13a249b8b8d61f7063
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 05:45:27 -0300

    Update HornHelmet.cs

--- script/item/defence/helm/HornHelmet.cs
+++ script/item/defence/helm/HornHelmet.cs
@@ -8,7 +8,7 @@ namespace LKCamelot.script.item
 		public override string Name { get { return "Horn Helmet"; } }
 
 		public override int DamBase { get { return 0; } }
-		public override int ACBase { get { return 0; } }
+		public override int ACBase { get { return 29; } }
 
 		public override int StrReq { get { return 206; } }
 		public override int DexReq { get { return 0; } }
@@ -16,7 +16,7 @@ namespace LKCamelot.script.item
 		public override int InitMinHits { get { return 525; } }
 		public override int InitMaxHits { get { return 525; } }
 
-		public override Class ClassReq { get { return 0; } }
+		public override Class ClassReq { get { return Class.Swordsman | Class.Shaman; } }
 		public override ArmorType ArmorType { get { return ArmorType.Helmet; } }
 
 		public HornHelmet() : base (4)
