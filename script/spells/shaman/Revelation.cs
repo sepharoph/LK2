3502c0f02ddd3c9a40ed1ef3f086ac6ad16372ef
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-06 21:41:39 -0300

    Update Revelation.cs

--- script/spells/shaman/Revelation.cs
+++ script/spells/shaman/Revelation.cs
@@ -6,11 +6,12 @@
         public override int SpellLearnedIcon { get { return 65; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
-        public override int DamBase { get { return 655; } }
-        public override int DamPl { get { return 15; } }
-        public override int ManaCost { get { return -80; } }
+        public override int DamBase { get { return 488; } }// compared to firehawk
+        public override int DamPl { get { return 12; } }//compared to firehawk
+        public override int ManaCost { get { return -77; } }//compared to firehawk
         public override int ManaCostPl { get { return 6; } }
         public override LKCamelot.library.Class ClassReq { get { return LKCamelot.library.Class.Shaman; } }
+        public override int menCoff { get { return 8; } }//compared to other class specific spells
         public override SpellSequence Seq
         {
             get
