dbb9b8163e2315720d25ed6b6d72ae40096c0898
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 00:13:31 -0300

    Update Crook.cs

--- script/item/weapons/cane/Crook.cs
+++ script/item/weapons/cane/Crook.cs
@@ -9,14 +9,16 @@ namespace LKCamelot.script.item
 
         public override int DamBase { get { return 66; } }
         public override int ACBase { get { return 44; } }
-
+        public override int InitMaxMana { get { return 65; } }//add max mana plz approve!!
+        
         public override int StrReq { get { return 33; } }
         public override int MenReq { get { return 152; } }
         public override int DexReq { get { return 0; } }
         public override int LevelReq { get { return 0; } }
+        
         public override int ReduceCast { get { return 600; } }
-        public override int InitMinHits { get { return 80; } }
-        public override int InitMaxHits { get { return 80; } }
+        public override int InitMinHits { get { return 0; } }//adds 0 hit
+        public override int InitMaxHits { get { return 0; } }//adds 0 hit
         public override int BuyPrice { get { return 100000; } }
 
         public override Class ClassReq { get { return Class.Wizard; } }
