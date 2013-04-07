b27fe9f950534a3a704668934dcf56abde1f5640
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-06 23:48:57 -0300

    Update Calypso.cs

--- script/item/weapons/cane/Calypso.cs
+++ script/item/weapons/cane/Calypso.cs
@@ -11,13 +11,14 @@ namespace LKCamelot.script.item
         public override int ACBase { get { return 130; } }
 
         public override int StrReq { get { return 180; } }
-        public override int MenReq { get { return 700; } }
+        public override int MenReq { get { return 650; } }
         public override int DexReq { get { return 0; } }
-
+        public override int LevelReq { get { return 220; } }//updated to reflect item is for 6th promo
+        
         public override int InitMinHits { get { return 80; } }
         public override int InitMaxHits { get { return 80; } }
         public override int BuyPrice { get { return 5000; } }
-        public override int ReduceCast { get { return 800; } }
+        public override int ReduceCast { get { return 1100; } }
 
         public override Class ClassReq { get { return Class.Wizard; } }
         public override WeaponType WeaponType { get { return WeaponType.Cane2; } }
