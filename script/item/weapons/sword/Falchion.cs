7ee74c668398164d64e4706aca110d767ed2d436
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 02:04:05 -0300

    Update Falchion.cs

--- script/item/weapons/sword/Falchion.cs
+++ script/item/weapons/sword/Falchion.cs
@@ -16,6 +16,8 @@ namespace LKCamelot.script.item
 		public override int InitMinHits { get { return 500; } }
 		public override int InitMaxHits { get { return 500; } }
 
+		public override int SellPrice { get { return 5000; } }
+
 		public override Class ClassReq { get { return Class.Knight | Class.Swordsman; } }
 		public override WeaponType WeaponType { get { return WeaponType.Sword; } }
 
