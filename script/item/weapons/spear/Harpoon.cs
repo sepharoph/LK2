cd4387cd7022befdc3e25af73e172dc228f9c44b
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 00:41:05 -0300

    Update Harpoon.cs

--- script/item/weapons/spear/Harpoon.cs
+++ script/item/weapons/spear/Harpoon.cs
@@ -8,12 +8,15 @@ namespace LKCamelot.script.item
         public override string Name { get { return "Harpoon"; } }
 
         public override int DamBase { get { return 69; } }
-        public override int ACBase { get { return 55; } }
-
+        public override int ACBase { get { return 64; } }
+        public override int InitMaxMana { get { return 73; } }//add max mana plz approve!!
+        
         public override int StrReq { get { return 102; } }
         public override int MenReq { get { return 231; } }
         public override int DexReq { get { return 141; } }
-        public override int ReduceCast { get { return 300; } }
+        public override int LevelReq { get { return 48; } }//reflects item
+        
+        public override int ReduceCast { get { return 900; } }// 9 dec time
         public override int InitMinHits { get { return 80; } }
         public override int InitMaxHits { get { return 80; } }
         public override int BuyPrice { get { return 5000; } }
