e3a2010fcb3027d014500ea9f23c6f9eca529fae
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 02:36:37 -0300

    Update DeadlyBoom.cs

--- script/spells/wizard/DeadlyBoom.cs
+++ script/spells/wizard/DeadlyBoom.cs
@@ -6,8 +6,8 @@
         public override int SpellLearnedIcon { get { return 69; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
-        public override int DamBase { get { return 444; } }//compared to big bang,assassin,revelation
-        public override int DamPl { get { return 12; } }
+        public override int DamBase { get { return 533; } }//compared to big bang,assassin,revelation
+        public override int DamPl { get { return 15; } }
         public override int ManaCost { get { return -65; } } //65
         public override int ManaCostPl { get { return 5; } }
         public override LKCamelot.library.Class ClassReq { get { return LKCamelot.library.Class.Wizard; } }
