b0029c27b3b059e7e9deb63694266592bc75e10f
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 02:06:18 -0300

    Update CoreaSword.cs

--- script/item/weapons/sword/CoreaSword.cs
+++ script/item/weapons/sword/CoreaSword.cs
@@ -15,6 +15,8 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 900; } }
 		public override int InitMaxHits { get { return 900; } }
+		
+		public override int SellPrice { get { return 40000; } }
 
 		public override Class ClassReq { get { return Class.Swordsman; } }
 		public override WeaponType WeaponType { get { return WeaponType.Sword; } }
