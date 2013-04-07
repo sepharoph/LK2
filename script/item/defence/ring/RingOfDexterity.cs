8d7b6b23832076846748bd738c05fad155db545e
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 18:58:54 -0300

    Create RingOfDexterity.cs

---
+++ script/item/defence/ring/RingOfDexterity.cs
@@ -0,0 +1,33 @@
+using LKCamelot.library;
+using LKCamelot.model;
+
+namespace LKCamelot.script.item
+{
+    public class RingOfDexterity : BaseArmor
+    {
+        public override string Name { get { return "Ring of Dexterity"; } }
+
+        public override int DamBase { get { return 0; } }
+        public override int ACBase { get { return 75; } }
+
+        public override int StrReq { get { return 0; } }
+        public override int DexReq { get { return 0; } }
+
+        public override int InitMinHits { get { return 65; } }
+        public override int InitMaxHits { get { return 65; } }
+        public override int BuyPrice { get { return 8000; } }
+
+        public override Class ClassReq { get { return 240; } }
+        public override ArmorType ArmorType { get { return ArmorType.Ring; } }
+
+        public RingOfDexterity()
+            : base(1)
+        {
+        }
+
+        public RingOfDexterity(Serial serial)
+            : base(serial)
+        {
+        }
+    }
+}
