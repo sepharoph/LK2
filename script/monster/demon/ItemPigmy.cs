af10e2ed59c66be4e7277e714939b99bedd8a4e1
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 03:58:55 -0300

    Update ItemPigmy.cs

--- script/monster/demon/ItemPigmy.cs
+++ script/monster/demon/ItemPigmy.cs
@@ -27,16 +27,16 @@ namespace LKCamelot.script.monster
                     new LootPackEntry(2, typeof(script.item.ThunderCrossBook), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.FlameRoundBook), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.TraceBook), "15d10+225", 1, 1, 1),
-
                     new LootPackEntry(2, typeof(script.item.Mantle), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.Rapier), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.Mask), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.Saber), "15d10+225", 1, 1, 1),
-                    new LootPackEntry(2, typeof(script.item.SpikedClub), "15d10+225", 1, 1, 1),
+                    new LootPackEntry(2, typeof(script.item.TraceBook), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.SquareShield), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.OneEdgedAxe), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.SmallAxe), "15d10+225", 1, 1, 1),
                     new LootPackEntry(2, typeof(script.item.Cape), "15d10+225", 1, 1, 1),
+                //  new LootPackEntry(2, typeof(script.item.ComebackBook), "15d10+225", 1, 1, 1),                        
                     new LootPackEntry(25.0, typeof(script.item.Gold), "5d10+800", 40, 1, 1),
                 });
             }
