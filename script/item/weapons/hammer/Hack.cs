e2be946e443d4873b2f02049784c2c144cba0af5
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 01:49:02 -0300

    Update Hack.cs

--- script/item/weapons/hammer/Hack.cs
+++ script/item/weapons/hammer/Hack.cs
@@ -15,6 +15,8 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 750; } }
 		public override int InitMaxHits { get { return 750; } }
+		
+		public override int SellPrice { get { return 80000; } }
 
 		public override Class ClassReq { get { return Class.Knight | Class.Swordsman; } }
 		public override WeaponType WeaponType { get { return WeaponType.Hammer; } }
