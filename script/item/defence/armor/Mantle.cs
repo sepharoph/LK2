f39bbc5b838dda669a72501fc6a5ee97bae4ce3b
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:18:49 -0300

    Update Mantle.cs

--- script/item/defence/armor/Mantle.cs
+++ script/item/defence/armor/Mantle.cs
@@ -15,8 +15,11 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 300; } }
 		public override int InitMaxHits { get { return 300; } }
+		
+		public override int BuyPrice { get { return 25000; } }
+    		public override int SellPrice { get { return 12500; } }
 
-        public override int APStage { get { return 1; } }
+     		public override int APStage { get { return 1; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
