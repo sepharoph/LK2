d8f727c67f65a6a8ce5dabf5f39a3983cc33ec44
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-08 00:04:57 -0300

    Update FieldCap.cs

--- script/item/defence/helm/FieldCap.cs
+++ script/item/defence/helm/FieldCap.cs
@@ -15,6 +15,8 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 720; } }
 		public override int InitMaxHits { get { return 720; } }
+		
+		public override int SellPrice { get { return 60000; } }
 
 		public override Class ClassReq { get { return Class.Knight | Class.Swordsman; } }
 		public override ArmorType ArmorType { get { return ArmorType.Helmet; } }
