0e92a539682231d2abeb80995c9ae6ef43c4637a
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-08 00:03:48 -0300

    Update Mask.cs

--- script/item/defence/helm/Mask.cs
+++ script/item/defence/helm/Mask.cs
@@ -15,7 +15,9 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 180; } }
 		public override int InitMaxHits { get { return 180; } }
-        public override int BuyPrice { get { return 50000; } }
+		
+        	public override int BuyPrice { get { return 10000; } }
+        	public override int SellPrice { get { return 5000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Helmet; } }
