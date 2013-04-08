06922f96effbb970cc80218ad10026f4ed0e3bb0
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:17:23 -0300

    Update Suit.cs

--- script/item/defence/armor/Suit.cs
+++ script/item/defence/armor/Suit.cs
@@ -15,7 +15,9 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 150; } }
 		public override int InitMaxHits { get { return 150; } }
-        public override int BuyPrice { get { return 5000; } }
+        	
+        	public override int BuyPrice { get { return 5000; } }
+       		public override int SellPrice { get { return 2500; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
