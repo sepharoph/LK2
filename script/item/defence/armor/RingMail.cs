a64ce3df2607c6d1b20548a52a544301e5fa1df4
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:10:52 -0300

    Update RingMail.cs

--- script/item/defence/armor/RingMail.cs
+++ script/item/defence/armor/RingMail.cs
@@ -16,7 +16,10 @@ namespace LKCamelot.script.item
 		public override int InitMinHits { get { return 990; } }
 		public override int InitMaxHits { get { return 990; } }
 
-        public override int APStage { get { return 2; } }
+        	public override int APStage { get { return 2; } }
+        
+        	public override int BuyPrice { get { return 140000; } }
+        	public override int SellPrice { get { return 70000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
