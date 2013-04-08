bcbcb4e583aabbc028d04ae647ca9c49b95a895a
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-08 00:02:30 -0300

    Update Hood.cs

--- script/item/defence/helm/Hood.cs
+++ script/item/defence/helm/Hood.cs
@@ -15,7 +15,9 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 30; } }
 		public override int InitMaxHits { get { return 30; } }
-        public override int BuyPrice { get { return 5000; } }
+        	
+        	public override int BuyPrice { get { return 1000; } }
+        	public override int SellPrice { get { return 500; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Helmet; } }
