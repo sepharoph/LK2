5488bc3bddea223b62c43e57437c32a33d762cbe
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:16:57 -0300

    Update Surplice.cs

--- script/item/defence/armor/Surplice.cs
+++ script/item/defence/armor/Surplice.cs
@@ -15,7 +15,9 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 220; } }
 		public override int InitMaxHits { get { return 220; } }
-        public override int BuyPrice { get { return 40000; } }
+		
+       		public override int BuyPrice { get { return 40000; } }
+       		public override int SellPrice { get { return 20000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
