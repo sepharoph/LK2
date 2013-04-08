0c8777ad823ac9bcc499752cf1ce8024e4eaf392
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:21:16 -0300

    Update Cope.cs

--- script/item/defence/armor/Cope.cs
+++ script/item/defence/armor/Cope.cs
@@ -15,6 +15,11 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 400; } }
 		public override int InitMaxHits { get { return 400; } }
+		
+		public override int BuyPrice { get { return 75000; } }
+        	public override int SellPrice { get { return 37500; } }
+        	
+        	public override int APStage { get { return 2; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
