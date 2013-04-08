24f0e9d923e71211c8c048963dca34990d8091db
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:18:26 -0300

    Update QuiltedArmor.cs

--- script/item/defence/armor/QuiltedArmor.cs
+++ script/item/defence/armor/QuiltedArmor.cs
@@ -15,11 +15,14 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 480; } }
 		public override int InitMaxHits { get { return 480; } }
+		
+		public override int BuyPrice { get { return 10000; } }
+     		public override int SellPrice { get { return 5000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
 
-        public override int APStage { get { return 1; } }
+       		public override int APStage { get { return 1; } }
 
 		public QuiltedArmor() : base (5)
 		{
