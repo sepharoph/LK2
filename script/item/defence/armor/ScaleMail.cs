dd06fb9ed30338642936579d70c0f02166aa9c4c
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:09:47 -0300

    Update ScaleMail.cs

--- script/item/defence/armor/ScaleMail.cs
+++ script/item/defence/armor/ScaleMail.cs
@@ -15,8 +15,11 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 1100; } }
 		public override int InitMaxHits { get { return 1100; } }
+		
+		public override int BuyPrice { get { return 160000; } }
+       		public override int SellPrice { get { return 80000; } }
 
-        public override int APStage { get { return 3; } }
+       		public override int APStage { get { return 3; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
