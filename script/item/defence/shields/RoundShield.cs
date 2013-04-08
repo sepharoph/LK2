d4d1465f325e977f12e233a95130829e003b07ef
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:54:03 -0300

    Update RoundShield.cs

--- script/item/defence/shields/RoundShield.cs
+++ script/item/defence/shields/RoundShield.cs
@@ -10,12 +10,14 @@ namespace LKCamelot.script.item
 		public override int DamBase { get { return 0; } }
 		public override int ACBase { get { return 8; } }
 
-		public override int StrReq { get { return 0; } }
+		public override int StrReq { get { return 15; } }
 		public override int DexReq { get { return 0; } }
 
 		public override int InitMinHits { get { return 80; } }
 		public override int InitMaxHits { get { return 80; } }
-        public override int BuyPrice { get { return 8000; } }
+        	
+        	public override int BuyPrice { get { return 2000; } }
+        	public override int SellPrice { get { return 1000; } }
 
 		public override Class ClassReq { get { return Class.Beginner | Class.Knight; } }
 		public override ArmorType ArmorType { get { return ArmorType.Shield; } }
