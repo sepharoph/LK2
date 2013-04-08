467eb51509f0734df122ae6308d5f931028a88f5
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:20:23 -0300

    Update EnamelLeather.cs

--- script/item/defence/armor/EnamelLeather.cs
+++ script/item/defence/armor/EnamelLeather.cs
@@ -15,8 +15,11 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 720; } }
 		public override int InitMaxHits { get { return 720; } }
+		
+		public override int BuyPrice { get { return 55000; } }
+        	public override int SellPrice { get { return 27500; } }
 
-        public override int APStage { get { return 2; } }
+        	public override int APStage { get { return 2; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
