e373164335c8e1921efe61c5ae60ded6face1379
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:18:00 -0300

    Update Rag.cs

--- script/item/defence/armor/Rag.cs
+++ script/item/defence/armor/Rag.cs
@@ -15,7 +15,9 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 80; } }
 		public override int InitMaxHits { get { return 80; } }
-        public override int BuyPrice { get { return 5000; } }
+		
+        	public override int BuyPrice { get { return 500; } }
+        	public override int SellPrice { get { return 250; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
