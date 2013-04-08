c36fc72b87bd761bbf200e825820c2c4d70ec572
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:16:14 -0300

    Update ToughLeather.cs

--- script/item/defence/armor/ToughLeather.cs
+++ script/item/defence/armor/ToughLeather.cs
@@ -15,7 +15,11 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 750; } }
 		public override int InitMaxHits { get { return 750; } }
-        public override int APStage { get { return 2; } }
+		
+        	public override int APStage { get { return 2; } }
+        	
+        	public override int BuyPrice { get { return 80000; } }
+      		public override int SellPrice { get { return 40000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
