53ac05b15732ef291513250f754db226306028ed
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:13:18 -0300

    Update BattleArmor.cs

--- script/item/defence/armor/BattleArmor.cs
+++ script/item/defence/armor/BattleArmor.cs
@@ -16,7 +16,10 @@ namespace LKCamelot.script.item
 		public override int InitMinHits { get { return 880; } }
 		public override int InitMaxHits { get { return 880; } }
 
-        public override int APStage { get { return 2; } }
+       		public override int APStage { get { return 2; } }
+        
+        	public override int BuyPrice { get { return 100000; } }
+       		public override int SellPrice { get { return 50000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
