89664389521512fa3677e8ea028ee627496f7054
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:19:55 -0300

    Update FullDress.cs

--- script/item/defence/armor/FullDress.cs
+++ script/item/defence/armor/FullDress.cs
@@ -15,7 +15,9 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 180; } }
 		public override int InitMaxHits { get { return 180; } }
-        public override int BuyPrice { get { return 15000; } }
+        	
+        	public override int BuyPrice { get { return 15000; } }
+        	public override int SellPrice { get { return 7500; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
