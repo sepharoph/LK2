7ad6c94ad735736f72bb4896332b504cfe9a6371
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:05:58 -0300

    Update FullPlate.cs

--- script/item/defence/armor/FullPlate.cs
+++ script/item/defence/armor/FullPlate.cs
@@ -16,7 +16,10 @@ namespace LKCamelot.script.item
 		public override int InitMinHits { get { return 1500; } }
 		public override int InitMaxHits { get { return 1500; } }
 
-        public override int APStage { get { return 3; } }
+     		public override int APStage { get { return 3; } }
+        
+      		public override int BuyPrice { get { return 400000; } }
+     		public override int SellPrice { get { return 200000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
