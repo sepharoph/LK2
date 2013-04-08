a12ef25ecc3e7136de457ceac0577fc59cabbf92
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:22:21 -0300

    Update Cloak.cs

--- script/item/defence/armor/Cloak.cs
+++ script/item/defence/armor/Cloak.cs
@@ -15,8 +15,11 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 360; } }
 		public override int InitMaxHits { get { return 360; } }
-
-        public override int APStage { get { return 1; } }
+		
+		public override int BuyPrice { get { return 85000; } }
+        	public override int SellPrice { get { return 47500; } }
+		
+		public override int APStage { get { return 1; } 
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
