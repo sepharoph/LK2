d82b76f83cc3bb5b4d5c0a33850ff44cefe13c4c
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-08 00:13:43 -0300

    Update RingOfVitality.cs

--- script/item/defence/ring/RingOfVitality.cs
+++ script/item/defence/ring/RingOfVitality.cs
@@ -18,6 +18,7 @@ namespace LKCamelot.script.item
         public override int InitMinHits { get { return 65; } }
         public override int InitMaxHits { get { return 65; } }
         public override int BuyPrice { get { return 8000; } }
+        public override int SellPrice { get { return 200000; } }
 
         public override Class ClassReq { get { return 240; } }//6th promo item
         public override ArmorType ArmorType { get { return ArmorType.Ring; } }
