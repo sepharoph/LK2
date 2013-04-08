a668f7388c8160a441f84560176255b7fa4543e1
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:14:51 -0300

    Update MailPlate.cs

--- script/item/defence/armor/MailPlate.cs
+++ script/item/defence/armor/MailPlate.cs
@@ -16,7 +16,10 @@ namespace LKCamelot.script.item
 		public override int InitMinHits { get { return 1200; } }
 		public override int InitMaxHits { get { return 1200; } }
 
-        public override int APStage { get { return 3; } }
+       		public override int APStage { get { return 3; } }
+        
+     		public override int BuyPrice { get { return 160000; } }
+     		public override int SellPrice { get { return 80000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
