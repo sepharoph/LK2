8b1a506cd28c7016f8ceb0d0f224b0d7149b91f5
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-08 00:03:00 -0300

    Update Cap.cs

--- script/item/defence/helm/Cap.cs
+++ script/item/defence/helm/Cap.cs
@@ -15,7 +15,9 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 65; } }
 		public override int InitMaxHits { get { return 65; } }
-        public override int BuyPrice { get { return 8000; } }
+		
+        	public override int BuyPrice { get { return 2000; } }
+        	public override int SellPrice { get { return 1000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Helmet; } }
