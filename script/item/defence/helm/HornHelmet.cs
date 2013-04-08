3575bbbb6ff3cc37c993dd0981b734357b7341bd
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-08 00:04:32 -0300

    Update HornHelmet.cs

--- script/item/defence/helm/HornHelmet.cs
+++ script/item/defence/helm/HornHelmet.cs
@@ -15,6 +15,8 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 525; } }
 		public override int InitMaxHits { get { return 525; } }
+		
+		public override int SellPrice { get { return 25000; } }
 
 		public override Class ClassReq { get { return Class.Swordsman | Class.Shaman; } }
 		public override ArmorType ArmorType { get { return ArmorType.Helmet; } }
