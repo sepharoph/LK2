6a0edb2fe8e4da0b4d13ccebd75b98fe5c7a542c
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:55:35 -0300

    Update ArmShield.cs

--- script/item/defence/shields/ArmShield.cs
+++ script/item/defence/shields/ArmShield.cs
@@ -15,6 +15,8 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 790; } }
 		public override int InitMaxHits { get { return 790; } }
+		
+		public override int SellPrice { get { return 25000; } }
 
 		public override Class ClassReq { get { return Class.Knight; } }
 		public override ArmorType ArmorType { get { return ArmorType.Shield; } }
