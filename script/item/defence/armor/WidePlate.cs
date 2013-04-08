8757173e1644092e79afbe920a5ecc869033d00a
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:15:24 -0300

    Update WidePlate.cs

--- script/item/defence/armor/WidePlate.cs
+++ script/item/defence/armor/WidePlate.cs
@@ -16,7 +16,10 @@ namespace LKCamelot.script.item
 		public override int InitMinHits { get { return 1400; } }
 		public override int InitMaxHits { get { return 1400; } }
 
-        public override int APStage { get { return 3; } }
+        	public override int APStage { get { return 3; } }
+        	
+        	public override int BuyPrice { get { return 190000; } }
+       		public override int SellPrice { get { return 95000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
