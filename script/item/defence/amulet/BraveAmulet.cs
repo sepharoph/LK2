5cfaa2a68d709ee82ed0b56267b71056f5aca7ea
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-08 00:15:50 -0300

    Update BraveAmulet.cs

--- script/item/defence/amulet/BraveAmulet.cs
+++ script/item/defence/amulet/BraveAmulet.cs
@@ -8,16 +8,17 @@ namespace LKCamelot.script.item
         public override string Name { get { return "Brave Amulet"; } }
 
         public override int DamBase { get { return 0; } }
-        public override int ACBase { get { return 0; } }
-// adds 100max MP
-// adds 50 HIT+DODGE
+        public override int ACBase { get { return 75; } }
+
         public override int StrReq { get { return 0; } }
         public override int DexReq { get { return 0; } }
-        public override int LevelReq { get { return 75; } }
+        public override int LevelReq { get { return 100; } }
 
         public override int InitMinHits { get { return 65; } }
         public override int InitMaxHits { get { return 65; } }
+        
         public override int BuyPrice { get { return 8000; } }
+        public override int SellPrice { get { return 100000; } }
 
         public override Class ClassReq { get { return 0; } }
         public override ArmorType ArmorType { get { return ArmorType.Amulet; } }
