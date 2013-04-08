0b0ddb211e4e3d9df64862ce111a50510719e5f1
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:11:45 -0300

    Update ChainMail.cs

--- script/item/defence/armor/ChainMail.cs
+++ script/item/defence/armor/ChainMail.cs
@@ -15,6 +15,9 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 1050; } }
 		public override int InitMaxHits { get { return 1050; } }
+		
+		public override int BuyPrice { get { return 145000; } }
+      		public override int SellPrice { get { return 72500; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
