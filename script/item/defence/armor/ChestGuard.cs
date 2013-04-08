cd5759dc703a12e3cdb9ed3bd4f3711be413c76b
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-07 23:14:28 -0300

    Update ChestGuard.cs

--- script/item/defence/armor/ChestGuard.cs
+++ script/item/defence/armor/ChestGuard.cs
@@ -15,7 +15,11 @@ namespace LKCamelot.script.item
 
 		public override int InitMinHits { get { return 770; } }
 		public override int InitMaxHits { get { return 770; } }
-        public override int APStage { get { return 2; } }
+		
+        	public override int APStage { get { return 2; } }
+        
+       		public override int BuyPrice { get { return 90000; } }
+        	public override int SellPrice { get { return 45000; } }
 
 		public override Class ClassReq { get { return 0; } }
 		public override ArmorType ArmorType { get { return ArmorType.Armor; } }
