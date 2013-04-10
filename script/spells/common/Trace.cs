3408ec07eac88118fd4fd5c33b93b4290735f582
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 00:47:14 -0300

    Update Trace.cs

--- script/spells/common/Trace.cs
+++ script/spells/common/Trace.cs
@@ -2,14 +2,20 @@
 {
     public class Trace : Spell
     {
-        public override string Name { get { return "TRACE"; } } //entrace of dun
+        public override string Name { get { return "TRACE"; } } 
         public override int SpellLearnedIcon { get { return 6; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Casted; } }
+
+/*This Spell Marked A Location on the map, when spell used aslong 
+as in the same map user would be brought to location 
+Leveling this spell reduces mana cost, starts at -70 mana, 
+(at 99:12 spell costs 20mana)
+*/
 
         public override int DamBase { get { return 0; } }
         public override int DamPl { get { return 0; } }
         public override int ManaCost { get { return 70; } }
-        public override int ManaCostPl { get { return 6; } }
+        public override int ManaCostPl { get { return 0; } }
 
         public override SpellSequence Seq
         {
