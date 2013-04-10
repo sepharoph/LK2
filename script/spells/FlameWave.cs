ff351748fdd426acd555818aa6260939ff2fcfef
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 01:11:39 -0300

    Update FlameWave.cs

--- script/spells/FlameWave.cs
+++ script/spells/FlameWave.cs
@@ -6,6 +6,12 @@
         public override int SpellLearnedIcon { get { return 16; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
+/*This effect expands from the caster in a Cross Shape AOE
+Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -98 Mana 
+(Max level 99-12 -36) Per Cast, Damage would Start (w/ 12 Books) 
+164 damage, (Max level 99-12 (164 damage) + 9%)
+This spell could be casted on all enemy Player's/Monster's. */
+
         public override int DamBase { get { return 58; } }
         public override int DamPl { get { return 6; } }
         public override int ManaCost { get { return 38; } }
