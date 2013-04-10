722dd85f5767f14db21ecb406ff17d50542775ca
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 05:06:44 -0300

    Update MagmaHand.cs

--- script/spells/shaman/MagmaHand.cs
+++ script/spells/shaman/MagmaHand.cs
@@ -6,6 +6,11 @@
         public override int SpellLearnedIcon { get { return 70; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
+/*Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -8% Mana 
+(Max level 99-12 -4%) Per Cast, Damage would Start (w/ 12 Books) 
+954 damage + menCoff, (Max level 99-12 (954damage + menCoff) + 9%)
+This spell could be casted on all enemy's. */
+
         public override int DamBase { get { return 778; } }//supposed to do 2nd most single target damage
         public override int DamPl { get { return 16; } }
         public override int ManaCost { get { return -96; } }//compared with other spells
