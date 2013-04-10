2ae9c797999a29fb18e69aafdd03ed429c1eb4df
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 01:13:24 -0300

    Update ThunderCross.cs

--- script/spells/ThunderCross.cs
+++ script/spells/ThunderCross.cs
@@ -11,6 +11,12 @@ namespace LKCamelot.script.spells
         public override int SpellLearnedIcon { get { return 24; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Casted; } }
 
+/* This Spell is supposed to fire lightning in the shape of a cross from the player.
+Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -105 Mana 
+(Max level 99-12 -55 Mana Per Cast) Per Cast, Damage would Start (w/ 12 Books) 
+210, (Max level 99-12 (210 damage) + 9%)
+This spell could be casted on all enemy Player's/Monster's using @pk feature for players. */
+
         public override int DamBase { get { return 132; } }
         public override int DamPl { get { return 8; } }
         public override int ManaCost { get { return 105; } }
