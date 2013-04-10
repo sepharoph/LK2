f97e0c6fe014d5732148273ab65dc3dcf4c15051
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-09 23:07:46 -0300

    Update DragonOfFire.cs

--- script/spells/wizard/DragonOfFire.cs
+++ script/spells/wizard/DragonOfFire.cs
@@ -6,7 +6,11 @@ namespace LKCamelot.script.spells
         public override int SpellLearnedIcon { get { return 64; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
-/* Summons Fire Dragon to attack Enemy, Similar to FireHawk*/
+/* Summons Fire Dragon to attack Enemy, Similar to FireHawk
+Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -12% Mana 
+(Max level 99-12 -8% Mana Per Cast) Per Cast, Damage would Start (w/ 12 Books) 
+709 damage + menCoff, (Max level 99-12 (709 damage + menCoff) + 9%)
+This spell could be casted only on all enemy's. */
 
         public override int DamBase { get { return 575; } }
         public override int DamPl { get { return 12; } }
