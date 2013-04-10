f730d3559b8a386f84d759ed56b2da74e24bc338
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 01:24:03 -0300

    Update Lightning.cs

--- script/spells/Lightning.cs
+++ script/spells/Lightning.cs
@@ -6,6 +6,12 @@
         public override int SpellLearnedIcon { get { return 72; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
+/* Shoots a Line of Electric at a target, the line is 3 squares long and 1 square thick.
+Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -106 Mana 
+(Max level 99:12 -56 Mana) Per Cast, Damage would Start (w/ 12 Books) 
+132 damage, (Max level 99:12 (132 damage + 9%)
+This spell could be casted on all enemy Player's/Monster's. */
+
         public override int DamBase { get { return 66; } }
         public override int DamPl { get { return 6; } }
         public override int ManaCost { get { return 40; } }
