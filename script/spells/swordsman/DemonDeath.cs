71cf5fbc5056c61c2ad3e908ccd3c9ca5891adbb
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 04:23:37 -0300

    Update DemonDeath.cs

--- script/spells/swordsman/DemonDeath.cs
+++ script/spells/swordsman/DemonDeath.cs
@@ -6,6 +6,13 @@
         public override int SpellLearnedIcon { get { return 68; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
+/* Demon Death dmg equation is (50%Max HP + DEX) Castable every 6 seconds for a cost of -15% HP, ALSO
++10seconds of Freezing Slow by 20%.
+Level of spell HP Consumed cost and increased damage, Starting at (w/ 12Books) -15% HP
+(Max level 99-12 -10% HP) Per Cast, Damage would Start (w/ 12 Books) 
+(50%Max HP + DEX), (Max level 99-12 (50%Max HP + DEX) + 9%)
+This spell could be casted on all enemy's. */ 
+
         public override int DamBase { get { return 0; } }
         public override int DamPl { get { return 0; } }
         public override int ManaCost { get { return -50; } }
