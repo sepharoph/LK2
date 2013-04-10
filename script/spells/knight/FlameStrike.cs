b0da4b3682b1e45a83dac29707fa1dec75d656e0
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 03:31:37 -0300

    Update FlameStrike.cs

--- script/spells/knight/FlameStrike.cs
+++ script/spells/knight/FlameStrike.cs
@@ -6,6 +6,11 @@
         public override int SpellLearnedIcon { get { return 67; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
+/*Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -34% Mana 
+(Max level 99-12 -30% Mana) Per Cast, Damage would Start (w/ 12 Books) 
+2550 damage + strCoff, (Max level 99-12 (2550damage + menCoff) + 9%)
+This spell could be casted on all enemy's. */
+
         public override int DamBase { get { return 2000; } }//updated
         public override int DamPl { get { return 50; } }//updated
         public override int ManaCost { get { return -100; } }//updated
