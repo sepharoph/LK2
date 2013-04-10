00f530eedafbaa7654be56a94f8e10f222175d79
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-09 23:53:22 -0300

    Update UltraBigBang.cs

--- script/spells/wizard/UltraBigBang.cs
+++ script/spells/wizard/UltraBigBang.cs
@@ -6,11 +6,16 @@
         public override int SpellLearnedIcon { get { return 64; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target; } }
 
+/*Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -10% Mana 
+(Max level 99-12 -5% Mana) Per Cast, Damage would Start (w/ 12 Books) 507 damage + menCoff, 
+(Max level 99-12 (507 damage + menCoff) + 9%) This spell could be casted on all 
+Enemy Players/Monster's. */
+
         public override int DamBase { get { return 397; } }//compared to grandbig bang
         public override int DamPl { get { return 11; } }
         public override int ManaCost { get { return -65; } }
         public override int ManaCostPl { get { return 5; } }
-        public override int menCoff { get { return 8; } }//compared to grand big bang
+        public override int menCoff { get { return 7; } }//compared to grand big bang
         public override int Range
         {
             get
