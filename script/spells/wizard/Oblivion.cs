e2c479b1279d7ccc2c49be323e8c39c664ed1c80
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-09 23:35:29 -0300

    Update Oblivion.cs

--- script/spells/wizard/Oblivion.cs
+++ script/spells/wizard/Oblivion.cs
@@ -6,8 +6,13 @@ namespace LKCamelot.script.spells
         public override int SpellLearnedIcon { get { return 71; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
-/*This spell should make a monster level 40 + (3 per book) lose agro to 
-player for 6 seconds and become confused, also please move to area for non class specific spells*/
+/*This spell should make a monster level 40 + (3 per book) lose agro to a
+player for 6 seconds and become confused would reset aggro upon duration end.
+/*Level of spell Increased Duration, Duration would start (w/ 12 Books) at
+6 seconds (at 99:12 lasts (6 seconds)+(50%) for total of lasting 9 seconds )
+This spell could be casted on all enemy Monsters's.
+(also please move to area for non class specific spells)
+*/
 
         public override int DamBase { get { return 0; } }
         public override int DamPl { get { return 0; } }
