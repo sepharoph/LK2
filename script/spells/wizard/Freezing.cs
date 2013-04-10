2a9cddf2c0150e6aef85c7be04325ad9d664f049
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 04:04:00 -0300

    Update Freezing.cs

--- script/spells/wizard/Freezing.cs
+++ script/spells/wizard/Freezing.cs
@@ -10,8 +10,8 @@
 (Max 10Seconds 99:12) for slow debuff Damage would Start (w/ 12 Books) 
 190 damage + menCoff, (Max level 99:12 (1950damage + menCoff) + 9%)
 This spell could be cast on All Enemy Players/Monsters. Also message was displayed 
-to the player when counting down to remove effect of slow 10,5-1 then says Freezing Slow Effect
-Removed */
+to the player when counting down to remove effect of freeze/immobilize at 
+10 Seconds left on Frozen Effect.,5,4,3,2,1 then says Frozen Effect Removed */
 
         public override int DamBase { get { return 80; } }
         public override int DamPl { get { return 10; } }
