1cf5e9b39a25cbbf330ff6f7c937fd21d0d439dd
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-09 23:21:01 -0300

    Update Freezing.cs

--- script/spells/wizard/Freezing.cs
+++ script/spells/wizard/Freezing.cs
@@ -6,16 +6,18 @@
         public override int SpellLearnedIcon { get { return 42; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
-/*Level of spell increased duration and damage, Starting at 4Seconds 
-(Max 10Seconds 99-12) for debuff (Mana Cost would give 4% Mana Back at Max Lvl 99-12).
-This spell could be casted only on self. Also message was displayed to the player 
-when counting down to remove effect of slow 10-1 then says Freezing Slow Effect
+/*Level of spell increased duration of slow and damage, Starting at 4Seconds 
+(Max 10Seconds 99-12) for slow debuff Damage would Start (w/ 12 Books) 
+190 damage + menCoff, (Max level 99-12 (1950damage + menCoff) + 9%)
+This spell could be cast on All Enemy Players/Monsters. Also message was displayed 
+to the player when counting down to remove effect of slow 10,5-1 then says Freezing Slow Effect
 Removed */
 
         public override int DamBase { get { return 80; } }
         public override int DamPl { get { return 10; } }
         public override int ManaCost { get { return -60; } }
         public override int ManaCostPl { get { return 4; } }
+        public override int menCoff { get { return 9; } }
         public override LKCamelot.library.Class ClassReq { get { return LKCamelot.library.Class.Wizard; } }
         public override SpellSequence Seq
         {
