57de3a21ba22135615fa6d82b9c56365f30de0a6
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 04:28:24 -0300

    Update Execution.cs

--- script/spells/swordsman/Execution.cs
+++ script/spells/swordsman/Execution.cs
@@ -6,6 +6,11 @@
         public override int SpellLearnedIcon { get { return 51; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
+/*Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -12% Mana 
+(Max level 99-12 -8%) Per Cast, Damage would Start (w/ 12 Books) 
+510 damage + dexCoff, (Max level 99-12 (510damage + dexCoff) + 9%)
+This spell could be casted on all enemy Player's/Monster's. */
+
         public override int DamBase { get { return 390; } }
         public override int DamPl { get { return 12; } }
         public override int ManaCost { get { return -67; } }
