a393056ec915c6072104a64237fb96b6fc9b5644
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-09 23:16:20 -0300

    Update CurveShock.cs

--- script/spells/wizard/CurveShock.cs
+++ script/spells/wizard/CurveShock.cs
@@ -7,7 +7,7 @@
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
 /*Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) +20% Mana 
-(Max level 99-12 +23% Mana Per Cast) Per Cast, Damage would Start (w/ 12 Books) 
+(Max level 99-12 +23% Mana) Per Cast, Damage would Start (w/ 12 Books) 
 1950 damage + menCoff, (Max level 99-12 (1950damage + menCoff) + 9%)
 This spell could be casted only on all enemy's. */
 
