ec279e5f09832fcf59872d19069d985fdead3227
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 03:57:45 -0300

    Update Radiation.cs

--- script/spells/shaman/Radiation.cs
+++ script/spells/shaman/Radiation.cs
@@ -6,6 +6,13 @@
         public override int SpellLearnedIcon { get { return 65; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
+/* Spell Decreased HP & AC by -10% book(s) 1-4((+)-10% 5-8, (+)-20% 9-12, Total of -30% (w/ 12Books) 
+removed from HP & AC, Level of spell reduced mana cost and increased duration, Starting at 3minutes 
+(Max 5 Minutes 99-12) for buff Mana Cost starts (w/ 12 books) -9% ,(-4% Mana Cost at Max Lvl 99-12).
+This spell could be casted on All Players/Monsters to debuff but not on self.
+Also message was displayed to the player when counting down to remove effect
+    30seconds than 10,5-1 then says Radiation Removed (Sold at Stall)*/
+
         public override int DamBase { get { return 0; } }
         public override int DamPl { get { return 0; } }
         public override int ManaCost { get { return -75; } }
