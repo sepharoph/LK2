3a7fd740d0830bcae2ada1e82a4bbffee389842a
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 00:11:47 -0300

    Update EvilMind.cs

--- script/spells/wizard/EvilMind.cs
+++ script/spells/wizard/EvilMind.cs
@@ -10,7 +10,7 @@ namespace LKCamelot.script.spells
 
 /*Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -312 Mana 
 (Max level 99-12 -180 Mana) Per Cast, Damage would Start (w/ 12 Books) 
- damage + menCoff, (Max level 99-12 (880 damage + menCoff) + 9%)
+880 damage + menCoff, (Max level 99-12 (880 damage + menCoff) + 9%)
 This spell could be casted on all Enemy Players/Monster's. */
 
 
@@ -18,7 +18,7 @@ This spell could be casted on all Enemy Players/Monster's. */
         public override int DamPl { get { return 20; } }
         public override int ManaCost { get { return 312; } }//fixed mana cost
         public override int ManaCostPl { get { return 0; } }
-        public override int menCoff { get { return 9; } }
+        public override int menCoff { get { return 8; } }
         public override int Range
         {
             get
