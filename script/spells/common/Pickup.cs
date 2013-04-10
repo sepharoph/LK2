466ef08e72f46ac5e3ee8dbf4a88d375026517e4
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 00:38:53 -0300

    Update Pickup.cs

--- script/spells/common/Pickup.cs
+++ script/spells/common/Pickup.cs
@@ -7,15 +7,14 @@
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Casted; } }
 
 /* Picks up all items in adjacent cells/squares 9 squares, current + 8 surrounding
-Leveling this spell decreases mana cost and increases distance of pickup
-from (w/ 12 books) -116 per cast (at 99:12 -66mana), distance includes 9 squares, current + 8
-(at 99:12 current + surrounding 40 cells/squares(that means 3 squares from the caster
-in every direction would have loot picked up) */
+Leveling this spell increases distance of pickup from, distance includes (w/ 12 books) 
+9 squares, current + 8(at 99:12 current + surrounding 40 cells/squares
+(that means 3 squares from the caster in every direction would have loot picked up) */
 
 
         public override int DamBase { get { return 0; } }
         public override int DamPl { get { return 0; } }
-        public override int ManaCost { get { return 116; } }
+        public override int ManaCost { get { return 0; } }
         public override int ManaCostPl { get { return 0; } }
 
         public override SpellSequence Seq
