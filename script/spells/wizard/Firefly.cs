f168a8ced4a3522326542d36d7f49712d7a90cc9
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-09 22:42:27 -0300

    Update Firefly.cs

--- script/spells/wizard/Firefly.cs
+++ script/spells/wizard/Firefly.cs
@@ -6,10 +6,17 @@
         public override int SpellLearnedIcon { get { return 71; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
+/* Spell Decreased HIT by 10%books 1-4(+10%5-8 +10% 9-12, Total of 30% at 12Books removed from HIT, 
+Level of spell reduced mana cost and increased duration, Starting at 3minutes (Max 5 Minutes)for debuff
+(Mana Cost would give 4% Mana Back at Max).
+This spell could be casted on players/Monsters to debuff but not on self.
+Also message was displayed to the player when counting down to remove effect
+    30seconds than 10-1 then says Firefly Removed*/
+
         public override int DamBase { get { return 0; } }
         public override int DamPl { get { return 0; } }
-        public override int ManaCost { get { return 0; } }
-        public override int ManaCostPl { get { return 6; } }
+        public override int ManaCost { get { return 48; } }
+        public override int ManaCostPl { get { return 4; } }
         public override LKCamelot.library.Class ClassReq { get { return LKCamelot.library.Class.Wizard; } }
         public override SpellSequence Seq
         {
