b9c7f3f736275e26aa4f298f2ea0db2197e92e30
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-09 23:22:46 -0300

    Update GrandBigBang.cs

--- script/spells/wizard/GrandBigBang.cs
+++ script/spells/wizard/GrandBigBang.cs
@@ -6,6 +6,11 @@
         public override int SpellLearnedIcon { get { return 64; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target; } }
 
+/*Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -6% Mana 
+(Max level 99-12 -4% Mana) Per Cast, Damage would Start (w/ 12 Books) 
+744 damage + menCoff, (Max level 99-12 (744 damage + menCoff) + 9%)
+This spell could be casted on all Enemy Players/Monster's. */
+
         public override int DamBase { get { return 612 } }//compared to assassin special closed gap
         public override int DamPl { get { return 12; } }//compared to assassin special closed gap
         public override int ManaCost { get { return -77; } }
