627a79994bea802ac72f26e91f634429b87bde88
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-09 23:08:57 -0300

    Update FireHawk.cs

--- script/spells/wizard/FireHawk.cs
+++ script/spells/wizard/FireHawk.cs
@@ -6,6 +6,11 @@
         public override int SpellLearnedIcon { get { return 14; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
+/*Level of spell decreased mana cost and increased damage, Starting at (w/ 12Books) -11% Mana 
+(Max level 99-12 -8% Mana Per Cast) Per Cast, Damage would Start (w/ 12 Books) 
+620 damage + menCoff, (Max level 99-12 (620damage + menCoff) + 9%)
+This spell could be casted only on all enemy's. */
+
         public override int DamBase { get { return 488; } }
         public override int DamPl { get { return 12; } }
         public override int ManaCost { get { return -77; } }
