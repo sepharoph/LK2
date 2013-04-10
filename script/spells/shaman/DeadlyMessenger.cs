0b383e508563c51fa20f852888176eb5ad3e2097
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 03:51:44 -0300

    Update DeadlyMessenger.cs

--- script/spells/shaman/DeadlyMessenger.cs
+++ script/spells/shaman/DeadlyMessenger.cs
@@ -6,6 +6,14 @@
         public override int SpellLearnedIcon { get { return 29; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
+/* Summons a Deadly Messenger, Looks like Monster, Is Demon, Has 310DMG (+10 per Book)
+80AC (+10 per Book) 
+
+Level of spell decreased mana cost and increased Wild Monk's Effectiveness, 
+Starting at (w/ 12Books) -22% Mana (Max level 99-12 -18% Mana) 
+Per Cast, Wild Monk would Start (w/ 12 Books) 420 DMG and 190 AC, (Max level 99:12 
+420 DMG and 190 AC (+20%) = 504DMG and 228 AC */
+
         public override int DamBase { get { return 310; } }
         public override int DamPl { get { return 10; } }
         public override int ManaCost { get { return -66; } }
