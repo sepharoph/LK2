a9abe8d4d96cfe32529202e4e516a9df4a77e82e
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 00:51:31 -0300

    Update Comeback.cs

--- script/spells/common/Comeback.cs
+++ script/spells/common/Comeback.cs
@@ -6,10 +6,13 @@
         public override int SpellLearnedIcon { get { return 54; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Casted; } }
 
+//on cast brings you to village.
+//Leveling this spell decreases mana cost starts at -70 (at 99:12 costs -20mana)
+
         public override int DamBase { get { return 0; } }
         public override int DamPl { get { return 0; } }
         public override int ManaCost { get { return 70; } }
-        public override int ManaCostPl { get { return 6; } }
+        public override int ManaCostPl { get { return 0; } }
 
         public override SpellSequence Seq
         {
