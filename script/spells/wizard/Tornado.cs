83fc4fa70ac94e9db992b1e4d9b28d31315e4e1c
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-09 22:32:04 -0300

    Update Tornado.cs

--- script/spells/wizard/Tornado.cs
+++ script/spells/wizard/Tornado.cs
@@ -6,7 +6,7 @@
         public override int SpellLearnedIcon { get { return 34; } }
         public override LKCamelot.library.MagicType mType { get { return LKCamelot.library.MagicType.Target2; } }
 
-/* Reduces Magic Damage By Monsters/Players by 10%*/
+/* Reduces Magic Damage By Monsters/Players by 10% lasts 3 minutes*/
 
         public override int DamBase { get { return 0; } }
         public override int DamPl { get { return 0; } }
