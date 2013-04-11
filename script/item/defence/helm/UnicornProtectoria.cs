32fdf3bcde9eb46f0de4633c31345378fbe6dc76
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-10 22:43:49 -0300

    Update UnicornProtectoria.cs

--- script/item/defence/helm/UnicornProtectoria.cs
+++ script/item/defence/helm/UnicornProtectoria.cs
@@ -8,11 +8,11 @@ namespace LKCamelot.script.item
         public override string Name { get { return "Unicorn Protectoria"; } }
 
         public override int DamBase { get { return 0; } }
-        public override int ACBase { get { return 100; } }
+        public override int ACBase { get { return 110; } }
 
         public override int StrReq { get { return 450; } }
-        public override int DexReq { get { return 5000; } }
-        public override int MenReq { get { return 7500; } }
+        public override int DexReq { get { return 1750; } }
+        public override int MenReq { get { return 3500; } }
         public override int VitReq { get { return 0; } }
         public override int LevelReq { get { return 240; } }
         
