abe095de81c0c4254990b326cfc907f6a84bfc67
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-12 01:48:51 -0300

    Create RingOfHealth.cs

---
+++ script/item/defence/ring/RingOfHealth.cs
@@ -0,0 +1,37 @@
+using LKCamelot.library;
+using LKCamelot.model;
+
+namespace LKCamelot.script.item
+{
+    public class RingOfHealth : BaseArmor
+    {
+        public override string Name { get { return "RING OF HEALTH"; } }
+
+        public override int DamBase { get { return 0; } }
+        public override int ACBase { get { return 0; } }
+
+        public override int StrReq { get { return 0; } }
+        public override int DexReq { get { return 0; } }
+        public override int StrBonus { get { return 8; } }//adds 8Str
+
+/*Actoz intended random chance for 6-10 STR added when this item dropped 
+per documentation, but was probably unable to code it.*/
+
+        public override int InitMinHits { get { return 65; } }
+        public override int InitMaxHits { get { return 65; } }
+        public override int BuyPrice { get { return 800; } }
+
+        public override Class ClassReq { get { return 0; } }
+        public override ArmorType ArmorType { get { return ArmorType.Ring; } }
+
+        public RingOfHealth()
+            : base(2)
+        {
+        }
+
+        public RingOfHealth(Serial serial)
+            : base(serial)
+        {
+        }
+    }
+}
