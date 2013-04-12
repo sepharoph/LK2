19f213cd00f550ebb8742ced5aad83d16ad092ff
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-12 03:18:51 -0300

    Create RingOfTheCraftsman.cs

---
+++ script/item/defence/ring/RingOfTheCraftsman.cs
@@ -0,0 +1,37 @@
+using LKCamelot.library;
+using LKCamelot.model;
+
+namespace LKCamelot.script.item
+{
+    public class RingOfTheCraftsman : BaseArmor
+    {
+        public override string Name { get { return "RING OF THE CRAFTSMEN"; } }
+
+        public override int DamBase { get { return 0; } }
+        public override int ACBase { get { return 0; } }
+
+        public override int StrReq { get { return 0; } }
+        public override int DexReq { get { return 0; } }
+        public override int DexBonus { get { return 36; } }//adds 36Dex
+
+/*Actoz intended random chance for 36-40 Dex added when this item dropped 
+per documentation, but was probably unable to code it.*/
+
+        public override int InitMinHits { get { return 65; } }
+        public override int InitMaxHits { get { return 65; } }
+        public override int BuyPrice { get { return 800; } }
+
+        public override Class ClassReq { get { return 0; } }
+        public override ArmorType ArmorType { get { return ArmorType.Ring; } }
+
+        public RingOfTheCraftsman()
+            : base(2)
+        {
+        }
+
+        public RingOfTheCraftsman(Serial serial)
+            : base(serial)
+        {
+        }
+    }
+}
