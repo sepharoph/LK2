acbbbd7f3a859a32092e227906fecde64014bc65
sepharoph <Jonathan.L.Lum@gmail.com>
2013-04-12 03:39:42 -0300

    Create RingOfTheSorcerer.cs

---
+++ script/item/defence/ring/RingOfTheSorcerer.cs
@@ -0,0 +1,37 @@
+using LKCamelot.library;
+using LKCamelot.model;
+
+namespace LKCamelot.script.item
+{
+    public class RingOfTheSorcerer : BaseArmor
+    {
+        public override string Name { get { return "RING OF THE SORCERER"; } }
+
+        public override int DamBase { get { return 0; } }
+        public override int ACBase { get { return 0; } }
+
+        public override int StrReq { get { return 0; } }
+        public override int DexReq { get { return 0; } }
+        public override int MenBonus { get { return 42; } }//adds 42Men
+
+/*Actoz intended random chance for 41-45 Men added when this item dropped 
+per documentation, but was probably unable to code it.*/
+
+        public override int InitMinHits { get { return 65; } }
+        public override int InitMaxHits { get { return 65; } }
+        public override int BuyPrice { get { return 800; } }
+
+        public override Class ClassReq { get { return 0; } }
+        public override ArmorType ArmorType { get { return ArmorType.Ring; } }
+
+        public RingOfTheSorcerer()
+            : base(2)
+        {
+        }
+
+        public RingOfTheSorcerer(Serial serial)
+            : base(serial)
+        {
+        }
+    }
+}
